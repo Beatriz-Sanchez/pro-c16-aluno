991a4cf3f8a8df09defe469e8af9f90c388070ab
Beatriz-Sanchez <40073484+Beatriz-Sanchez@users.noreply.github.com>
2021-12-06 12:21:32 -0300

    Update caixa.js

--- caixa.js
+++ caixa.js
@@ -1,1 +1,11 @@
-
+class Caixa {
+    constructor(){
+        this.x = 100;
+        this.y = 200;
+        this.w = 50;
+        this.h = 50;
+    }
+}
+mostrar(){
+    rect(this.x, this.y, this.w, this.h);
+}
